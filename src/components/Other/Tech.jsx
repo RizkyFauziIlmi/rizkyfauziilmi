ef29004b303b54582bccd657551fc9b2895586d6
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-07-06 23:18:30 +0700

    theme color and discredible link fix

--- src/components/Other/Tech.jsx
+++ src/components/Other/Tech.jsx
@@ -22,7 +22,7 @@ export default function Tech() {
     return(
         <Breadcrumb separator="-" textAlign={'center'}>
             <BreadcrumbItem ml={2}>
-                <BreadcrumbLink href='https://reactjs.org' target={'_blank'}>
+                <BreadcrumbLink className="react-website" href='https://reactjs.org' target={'_blank'}>
                     <Flex alignItems={'center'} gap={1}>
                         <Flex animation={circleAnimation("reverse")}>
                             <FaReact /> 
@@ -32,7 +32,7 @@ export default function Tech() {
                 </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbItem>
-                <BreadcrumbLink href='https://chakra-ui.com' target={'_blank'}>
+                <BreadcrumbLink className="chakra-webiste" href='https://chakra-ui.com' target={'_blank'}>
                     <Flex alignItems={'center'} gap={1}>
                         <SiChakraui/> 
                         <Text>Chakra UI</Text>
@@ -40,7 +40,7 @@ export default function Tech() {
                 </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbItem>
-                <BreadcrumbLink href='https://www.vercel.com/' target={'_blank'}>
+                <BreadcrumbLink className="vercel-website" href='https://www.vercel.com/' target={'_blank'}>
                     <Flex alignItems={'center'} gap={1}>
                         <SiVercel /> 
                         <Text>Vercel</Text>
@@ -48,7 +48,7 @@ export default function Tech() {
                 </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbItem>
-                <BreadcrumbLink href='https://www.npmjs.com/' target={'_blank'}>
+                <BreadcrumbLink className="npm-website" href='https://www.npmjs.com/' target={'_blank'}>
                     <Flex alignItems={'center'} gap={1}>
                         <FaNpm/> 
                         <Text>Npm</Text>

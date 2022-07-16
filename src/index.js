bc277f84109b24b19612183cd3d87f9d7c49e3ca
Rizky Fauzi Ilmi <104153142+RizkyFauziIlmi@users.noreply.github.com>
2022-07-16 13:16:14 +0700

    import font, font size heading fixed

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ import '@fontsource/source-code-pro/600.css'
 import '@fontsource/source-code-pro/700.css'
 import '@fontsource/source-code-pro/800.css'
 import '@fontsource/source-code-pro/900.css'
+import '@fontsource/source-code-pro/200-italic.css'
+import '@fontsource/source-code-pro/300-italic.css'
+import '@fontsource/source-code-pro/400-italic.css'
+import '@fontsource/source-code-pro/500-italic.css'
+import '@fontsource/source-code-pro/600-italic.css'
+import '@fontsource/source-code-pro/700-italic.css'
+import '@fontsource/source-code-pro/800-italic.css'
+import '@fontsource/source-code-pro/900-italic.css'
 
 
 import Calculator from './components/calculator/Calculator';
